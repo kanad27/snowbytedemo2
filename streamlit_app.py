cfd7f175fdb80919c1a444d49493ef7b575db4c3
kanad27 <kanad27@gmail.com>
2024-08-12 17:05:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 import pydeck as pdk
 from vega_datasets import data
-from snowflake.snowpark.context import get_active_session
+
 
 # Write directly to the app
 st.title("Example Streamlit App :balloon:")
@@ -18,7 +18,7 @@ st.write(
 st.write(f"""Streamlit Version {st.__version__}""")
 
 # Get the current credentials
-session = get_active_session()
+#session = get_active_session()
 
 # Area Chart
 chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
