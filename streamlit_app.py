9ebb63ee17539a85198366a051dcdfb74de5b747
kanad27 <kanad27@gmail.com>
2024-08-12 17:06:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 import pydeck as pdk
 from vega_datasets import data
-
+# from snowflake.snowpark.context import get_active_session
 
 # Write directly to the app
 st.title("Example Streamlit App :balloon:")
