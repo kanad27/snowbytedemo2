96e64bbd2ea20cfe20fb80c51e5081dd69a0bbba
kanad27 <kanad27@gmail.com>
2025-06-22 21:16:16 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import pydeck as pdk
+# import pydeck as pdk
 from vega_datasets import data
 # from snowflake.snowpark.context import get_active_session
 
@@ -44,39 +44,41 @@ st.write(f"""Example of 3D Map""")
 
 chart_data = pd.DataFrame(
     np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-    columns=["lat", "lon"],
+    columns=["lat", "long"],
 )
 
-st.pydeck_chart(
-    pdk.Deck(
-        map_style=None,
-        initial_view_state=pdk.ViewState(
-            latitude=37.76,
-            longitude=-122.4,
-            zoom=11,
-            pitch=50,
-        ),
-        layers=[
-            pdk.Layer(
-                "HexagonLayer",
-                data=chart_data,
-                get_position="[lon, lat]",
-                radius=200,
-                elevation_scale=4,
-                elevation_range=[0, 1000],
-                pickable=True,
-                extruded=True,
-            ),
-            pdk.Layer(
-                "ScatterplotLayer",
-                data=chart_data,
-                get_position="[lon, lat]",
-                get_color="[200, 30, 0, 160]",
-                get_radius=200,
-            ),
-        ],
-    )
-)
+# Enable import pydeck as pdk to use pydecj chart
+
+# st.pydeck_chart(
+#     pdk.Deck(
+#         map_style=None,
+#         initial_view_state=pdk.ViewState(
+#             latitude=37.76,
+#             longitude=-122.4,
+#             zoom=11,
+#             pitch=50,
+#         ),
+#         layers=[
+#             pdk.Layer(
+#                 "HexagonLayer",
+#                 data=chart_data,
+#                 get_position="[lon, lat]",
+#                 radius=200,
+#                 elevation_scale=4,
+#                 elevation_range=[0, 1000],
+#                 pickable=True,
+#                 extruded=True,
+#             ),
+#             pdk.Layer(
+#                 "ScatterplotLayer",
+#                 data=chart_data,
+#                 get_position="[lon, lat]",
+#                 get_color="[200, 30, 0, 160]",
+#                 get_radius=200,
+#             ),
+#         ],
+#     )
+# )
 
 # Map
 
